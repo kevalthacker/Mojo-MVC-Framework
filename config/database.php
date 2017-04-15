b2d9528f1c24765a672df7272cd6ee99767bc480
Keval Thacker <kevalthacker@gmail.com>
2017-04-15 20:09:38 +0530

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,12 @@
+<?php
+/*********************************
+MANAGEMANT SYSTEM : CUSTOM MVC FRAMWORK - DATABASE SETTING
+VERSION : 1.0 BETA
+INITIALIZE DATABASE SETTINGS
+***********/
+define ('db_type', 'MYSQL');
+define ('db_host', 'localhost');
+define ('db_port', '');
+define ('db_name', 'management_system');
+define ('db_user', 'root');
+define ('db_pass', '');
