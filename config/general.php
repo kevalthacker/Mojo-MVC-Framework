692bff35e4f45250534bdfa5ae5956f4fd9ae892
Keval Thacker <kevalthacker@gmail.com>
2017-04-15 20:07:04 +0530

    Create general.php

---
+++ config/general.php
@@ -0,0 +1,17 @@
+<?php
+/*********************************
+MANAGEMANT SYSTEM : CUSTOM MVC FRAMWORK - GENRAL SETTING
+VERSION : 1.0 BETA
+INITIALIZE ALL REQUIRED GENERAL SETTINGS
+***********/
+session_start();
+define('URL_PROTOCOL', '//');
+define('URL_DOMAIN', $_SERVER['HTTP_HOST']);
+define('URL', URL_PROTOCOL.URL_DOMAIN."/framework");
+define ('sitename', 'Adroit Technosys');
+define ('primary_colorcode', '#1a4b76');
+define ('logoname', 'logo.png');
+define ('pdf_library', 'dompdf');
+define ('db_prefix', '');
+define ('error_msg', '<div class="alert alert-error"><strong>Error!</strong> %msg% </div>');
+define ('success_msg', '<div class="alert alert-success"><strong>Success!</strong> %msg% </div>');
