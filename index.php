cc8afc44111ffa7e37f66056ab98fdbf6d0580e7
Keval Thacker <kevalthacker@gmail.com>
2017-04-15 20:08:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 /*********************************
-MANAGEMANT SYSTEM : CUSTOM MVC FRAMWORK
+MOJO MVC FRAMEWORK
 VERSION : 1.0 BETA
 DEVELOPED BY : KEVAL THACKER
 WEBSITE : http://adroittechnosys.com/
 ***********/
 // DIRECTORY_SEPARATOR adds a slash to the end of the path
-define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR."framework". DIRECTORY_SEPARATOR);
 // set a constant that holds the project's "config" folde, like.
 define('CONFIG', ROOT . 'config' . DIRECTORY_SEPARATOR);
 // set a constant that holds the project's "lib" folder
@@ -15,7 +15,7 @@ define('LIB', ROOT . 'lib' . DIRECTORY_SEPARATOR);
 define('ASSETS', ROOT . 'assets' . DIRECTORY_SEPARATOR);
 // set a constant that holds the project's "current Active Page" 
 define ('FILE_NAME', basename(strtok($_SERVER["REQUEST_URI"],'?')));
-define ('FILE_PATH', strtok($_SERVER["REQUEST_URI"],'?'));
+define ('FILE_PATH', str_replace("/framework/","",strtok($_SERVER["REQUEST_URI"],'?')));
 /**************************************
 ***************************************
 ***************************************
