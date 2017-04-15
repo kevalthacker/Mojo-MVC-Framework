64ca8364192f1474bca35451e5016bb90f407344
Keval Thacker <kevalthacker@gmail.com>
2017-04-15 20:03:30 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/*********************************
+MANAGEMANT SYSTEM : CUSTOM MVC FRAMWORK
+VERSION : 1.0 BETA
+DEVELOPED BY : KEVAL THACKER
+WEBSITE : http://adroittechnosys.com/
+***********/
+// DIRECTORY_SEPARATOR adds a slash to the end of the path
+define('ROOT', dirname(__DIR__) . DIRECTORY_SEPARATOR);
+// set a constant that holds the project's "config" folde, like.
+define('CONFIG', ROOT . 'config' . DIRECTORY_SEPARATOR);
+// set a constant that holds the project's "lib" folder
+define('LIB', ROOT . 'lib' . DIRECTORY_SEPARATOR);
+// set a constant that holds the project's "assets" folder
+define('ASSETS', ROOT . 'assets' . DIRECTORY_SEPARATOR);
+// set a constant that holds the project's "current Active Page" 
+define ('FILE_NAME', basename(strtok($_SERVER["REQUEST_URI"],'?')));
+define ('FILE_PATH', strtok($_SERVER["REQUEST_URI"],'?'));
+/**************************************
+***************************************
+***************************************
+   LOAD GENERAL SETTING CLASS
+***************************************
+***************************************  
+***/
+include(CONFIG."general.php");
+/**************************************
+***************************************
+***************************************
+   LOAD DATABASE SETTING CLASS
+***************************************
+***************************************  
+***/
+include(CONFIG."database.php");
+// load initialize application
+include LIB."init.php";
